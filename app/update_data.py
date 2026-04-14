b5eaa6cb8e6b7f9f6b8128ded39494b255973837
VDJ2824 <142669691+VDJ2824@users.noreply.github.com>
2026-04-14 15:30:38 +0530

    Add files via upload

---
+++ app/update_data.py
@@ -0,0 +1,10 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()
+
+from app.scopus_service import update_all_faculty_data
+
+if __name__ == "__main__":
+    update_all_faculty_data()
