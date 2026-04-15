1b7e0ab7fc17e4e6f791291a3dc9fce53a0c27b5
VDJ2824 <142669691+VDJ2824@users.noreply.github.com>
2026-04-15 18:24:17 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,24 +3,20 @@ import json
 import firebase_admin
 from firebase_admin import credentials, db
 
-# Load Firebase credentials from environment variable
-firebase_key = os.environ.get("FIREBASE_KEY")
+firebase_json = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
 
-if not firebase_key:
-    raise ValueError("FIREBASE_KEY environment variable not set")
+if not firebase_json:
+    raise ValueError("FIREBASE_SERVICE_ACCOUNT not set")
 
-# Convert string → JSON
-firebase_config = json.loads(firebase_key)
+firebase_config = json.loads(firebase_json)
 
-cred = credentials.Certificate(firebase_config)
+if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_config)
 
-# Initialize app
-firebase_admin.initialize_app(cred, {
-    'databaseURL': os.environ.get(
-        'FIREBASE_DATABASE_URL',
-        'https://research-paper-webscrapp-e6d2b-default-rtdb.asia-southeast1.firebasedatabase.app/'
-    )
-})
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': os.environ.get('FIREBASE_DATABASE_URL')
+    })
+
 
 def get_db():
     return db.reference()
