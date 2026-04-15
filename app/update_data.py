0e72052b4faa49ceb0690e4d9b81d4e75d28e986
VDJ2824 <142669691+VDJ2824@users.noreply.github.com>
2026-04-15 19:29:24 +0530

    Update update_data.py

--- app/update_data.py
+++ app/update_data.py
@@ -4,7 +4,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
-from app.scopus_service import update_all_faculty_data
+from scopus_service import update_all_faculty_data
 
 if __name__ == "__main__":
     update_all_faculty_data()
