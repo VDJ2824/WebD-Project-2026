965b2b3898a3cfe4bd04e9fcd090ee12f3a1df3d
VDJ2824 <142669691+VDJ2824@users.noreply.github.com>
2026-04-15 18:49:04 +0530

    Update firebase_config.py

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -3,18 +3,21 @@ import json
 import firebase_admin
 from firebase_admin import credentials, db
 
-firebase_json = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+db_url = os.getenv("FIREBASE_DATABASE_URL")
 
 if not firebase_json:
-    raise ValueError("FIREBASE_SERVICE_ACCOUNT not set")
+    raise ValueError("Missing FIREBASE_SERVICE_ACCOUNT")
+
+if not db_url:
+    raise ValueError("Missing FIREBASE_DATABASE_URL")
 
 firebase_config = json.loads(firebase_json)
 
 if not firebase_admin._apps:
     cred = credentials.Certificate(firebase_config)
-
     firebase_admin.initialize_app(cred, {
-        'databaseURL': os.environ.get('FIREBASE_DATABASE_URL')
+        "databaseURL": db_url
     })
 
 
