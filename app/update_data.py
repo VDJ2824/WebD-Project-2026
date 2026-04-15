a9e1828e3e2eb5f7c6e460bab4e92ece324e7690
VDJ2824 <142669691+VDJ2824@users.noreply.github.com>
2026-04-15 19:39:05 +0530

    Update update_data.py

--- app/update_data.py
+++ app/update_data.py
@@ -1,6 +1,7 @@
 import os
-import django
+import sys
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
