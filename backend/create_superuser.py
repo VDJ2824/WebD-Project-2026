d942f23e58c7bbd378072d1d80cc6ac03e366315
VDJ2824 <142669691+VDJ2824@users.noreply.github.com>
2026-04-15 18:56:17 +0530

    Add files via upload

---
+++ backend/create_superuser.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+
+username = "Varima"
+password = "Vdj@2824"
+email = "admin@example.com"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created")
+else:
+    print("Superuser already exists")
